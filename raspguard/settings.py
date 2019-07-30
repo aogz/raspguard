562793e71c31bc7e2f548ceb2a3d2a954bff0ab6
Alexander Ogurtsov <aogurzow@gmail.com>
2019-07-30 19:55:07 +0300

    path to gifs fixed

--- raspguard/settings.py
+++ raspguard/settings.py
@@ -1,3 +1,7 @@
+import os 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SENSITIVITY = 30  # 1 - 100
 MIN_CONTOUR = 5000
 TG_BOT_API_KEY = ''
@@ -6,6 +10,7 @@ GIF_FOLDER = 'gifs'
 DATETIME_FORMAT = "%d-%m-%Y %H:%M:%S"
 GIF_FPS = 3
 MAX_FRAMES = 100
+GIF_FOLDER = os.path.join(BASE_DIR, 'gifs')
 
 try:
     from .settings_local import *
